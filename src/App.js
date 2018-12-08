47ac690b5fa417d111ee8aa68ba2652039c0a317
Phat Ma <phat.ma@hotmail.co.uk>
2018-12-08 21:17:01 +0000

    prevent whitespaces

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function TodoForm({ addTodo }) {
 
   const handleSubmit = e => {
     e.preventDefault();
-    if (!value) return;
+    if (!value.trim()) return;
     addTodo(value);
     setValue('');
   };
